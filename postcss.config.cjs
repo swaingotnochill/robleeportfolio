5e39d168db9741ee25e3e8042350b42cee084250
Roshan Swain <swainroshan001@gmail.com>
2023-01-07 12:32:40 +0530

    init : sanity

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      content: ["./src/**/*.{js,ts,jsx,tsx}"],
+      theme: {
+      extend: {},
+  },
+      plugins: [],
+  },
     autoprefixer: {},
   },
 };
